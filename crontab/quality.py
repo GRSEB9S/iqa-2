55caf5e74d6e26dcdd552e3f9ac85cd24cb8ec6a
for services <services@ip-172-31-30-25.us-west-2.compute.internal>
2018-01-13 10:35:21 +0000

    minor

--- crontab/quality.py
+++ crontab/quality.py
@@ -21,30 +21,29 @@ except:
 
 docs = []
 
-for d in cl.find({'lastUpdate':{'$gte':last_ts}}):
+for d in cl.find({'lastUpdate':{'$gte':last_ts}}).limit(10000):
   docs.append(d)
 
 def process(d):
   docid = d['_id']
-  try:
-    if 'quality' in d:
-      raise Exception('quality exists')
-    r = requests.get('http://internal-a4api-74034796.us-west-2.elb.amazonaws.com/Website/contents/content?docid=%s&fields=image' % docid)
-    j = json.loads(r.content)
-    if 'documents' not in j:
-      raise Exception('docid not exists')
-    if 'image' not in j['documents'][0]:
-      raise Exception('image not exists')
-    imgid = j['documents'][0]['image']
-    r = requests.get('http://internal-a4api-74034796.us-west-2.elb.amazonaws.com/Website/video/image-quality-check?image=%s' % imgid)
-    j = json.loads(r.content)
-    q = j['result'][imgid]['quality']
-    cl.update({'_id':docid},{'$set':{'quality':q}})
-  except Exception, e:
-    print when(), 'ERROR:',e,docid
+  if 'quality' not in d:
+    try:
+      r = requests.get('http://internal-a4api-74034796.us-west-2.elb.amazonaws.com/Website/contents/content?docid=%s&fields=image' % docid)
+      j = json.loads(r.content)
+      if 'documents' not in j:
+        raise Exception('docid not exists')
+      if 'image' not in j['documents'][0]:
+        raise Exception('image not exists')
+      imgid = j['documents'][0]['image']
+      r = requests.get('http://internal-a4api-74034796.us-west-2.elb.amazonaws.com/Website/video/image-quality-check?image=%s' % imgid)
+      j = json.loads(r.content)
+      q = j['result'][imgid]['quality']
+      cl.update({'_id':docid},{'$set':{'quality':q}})
+    except Exception, e:
+      print when(), 'ERROR:',e,docid
   return d['lastUpdate']
 
-pool = ThreadPool(10)
+pool = ThreadPool(20)
 results = pool.map(process, docs)
 pool.close()
 pool.join()
