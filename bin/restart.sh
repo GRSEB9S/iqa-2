d611b520e4d0b7f2a66080bf8c47168a616cb291
hay86 <xukaifeng1986@gmail.com>
2018-03-21 03:30:47 -0700

    append log

--- bin/restart.sh
+++ bin/restart.sh
@@ -3,4 +3,4 @@
 set -x
 kill -9 $(ps aux | grep "Image-Quality-Assessment-Model" | grep -v grep | awk '{print $2}')
 root=/home/services/kaifeng/iqa
-nohup python $root/src/server.py $root/data/Image-Quality-Assessment-Model > $root/logs/nohup.log 2>&1 &
+nohup python $root/src/server.py $root/data/Image-Quality-Assessment-Model >> $root/logs/nohup.log 2>&1 &
