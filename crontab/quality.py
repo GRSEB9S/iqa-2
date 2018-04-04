defc7d20d3b77b62c4078d54826ff949a3f384d8
hay86 <xukaifeng1986@gmail.com>
2018-04-04 08:15:44 -0700

    use mongo to retrieve doc

--- crontab/quality.py
+++ crontab/quality.py
@@ -15,47 +15,53 @@ if __name__ == '__main__':
         print 'Usage: python quality.py last_ts.txt'
         sys.exit()
 
-    with MongoClient('172.31.23.34:27017,172.24.22.248:27017', replicaset='rs-userprofile') as c:
-        last_ts = int(time.time() - 72 * 3600)
-        with open(sys.argv[1]) as fin:
-            last_ts = int(fin.read())
+    last_ts = int(time.time() - 72 * 3600)
+    with open(sys.argv[1]) as fin:
+        last_ts = int(fin.read())
 
-        cl = c['documentLabels']['poi']
-        docs = [d for d in cl.find({'lastUpdate': {'$gte': last_ts}}).limit(20000)]
+    c1 = MongoClient('mongodb://172.31.23.34:27017,172.24.22.248:27017/?replicaset=rs-userprofile')
+    c2 = MongoClient('mongodb://172.31.20.160:27017,172.31.20.161:27017,172.31.20.162:27017/?replicaSet=rs2')
 
-        def process(d):
-            docid = d['_id']
-            if 'quality' not in d:
-                try:
-                    r = requests.get('http://a4api.ha.nb.com/Website/contents/content?docid=%s&fields=image' % docid)
+    cr1 = c1['documentLabels']['poi']
+    cr2 = c2['serving']['displayDocument']
+
+    with cr1.find({'lastUpdate': {'$gte': last_ts}}).limit(20000) as result:
+        docs = [d for d in result]
+
+    def process(d):
+        if 'quality' not in d:
+            try:
+                one = cr2.find_one({'_id': d['_id']}, {'image': 1})
+                if not one:
+                    sys.stderr.write('(%s) ERROR: docid not exists: %s\n' % (when(), d['_id']))
+                    q = 'high'
+                elif not one.get('image', False):
+                    sys.stderr.write('(%s) ERROR: image not exists: %s\n' % (when(), d['_id']))
+                    q = 'high'
+                else:
+                    imgid = one['image']
+                    r = requests.get('http://a4api.ha.nb.com/Website/image/quality-check?image=%s' % imgid)
                     j = json.loads(r.content)
-                    if 'documents' not in j:
-                        sys.stderr.write('(%s) ERROR: docid not exists: %s\n' % (when(), docid))
-                        q = 'high'
-                    elif 'image' not in j['documents'][0]:
-                        sys.stderr.write('(%s) ERROR: image not exists: %s\n' % (when(), docid))
-                        q = 'high'
-                    else:
-                        imgid = j['documents'][0]['image']
-                        r = requests.get('http://a4api.ha.nb.com/Website/image/quality-check?image=%s' % imgid)
-                        j = json.loads(r.content)
-                        q = j['result'][imgid]['quality']
-                    cl.update({'_id':docid},{'$set':{'quality':q}})
-                except Exception, e:
-                    t, o, tb = sys.exc_info()
-                    f = tb.tb_frame
-                    lineno = tb.tb_lineno
-                    filename = f.f_code.co_filename
-                    sys.stderr.write('(%s) ERROR: Exception in %s, Line %s: %s, docid %s\n' % (when(), filename, str(lineno), e, docid))
-            return d['lastUpdate']
+                    q = j['result'][imgid]['quality']
+                cr1.update({'_id': d['_id']}, {'$set': {'quality': q}})
+            except Exception, e:
+                t, o, tb = sys.exc_info()
+                f = tb.tb_frame
+                lineno = tb.tb_lineno
+                filename = f.f_code.co_filename
+                sys.stderr.write('(%s) ERROR: Exception in %s, Line %s: %s, docid %s\n' % (when(), filename, str(lineno), e, d['_id']))
+        return d['lastUpdate']
 
-        pool = ThreadPool(20)
-        results = pool.map(process, docs)
-        pool.close()
-        pool.join()
+    pool = ThreadPool(20)
+    results = pool.map(process, docs)
+    pool.close()
+    pool.join()
 
-        if len(results) > 0:
-            with open(sys.argv[1], 'w') as fout:
-                fout.write(str(max(results)))
+    c1.close()
+    c2.close()
 
-        print when(), 'INFO:', len(results), 'processed'
+    if len(results) > 0:
+        with open(sys.argv[1], 'w') as fout:
+            fout.write(str(max(results)))
+
+    print when(), 'INFO:', len(results), 'processed'
